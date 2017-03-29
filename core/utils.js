b410aea674128d1da94193cc4b159bf2d09fc30c
Olivier Brassard <obrassard@amplify.com>
2017-03-29 11:31:22 -0400

    [CSI-157] - added stack trace

--- core/utils.js
+++ core/utils.js
@@ -54,6 +54,7 @@ Blockly.cache3dSupported_ = null;
  * @return {boolean} True if class was added, false if already present.
  */
 Blockly.utils.addClass = function(element, className) {
+  console.trace();
   var classes = element.getAttribute('class') || '';
   if ((' ' + classes + ' ').indexOf(' ' + className + ' ') != -1) {
     return false;
